8a8993a395170d083cd386b231e4b679a4ae5c3b
thepedroferrari <pedro@pedroferrari.com>
2021-09-05 18:12:49 +0200

    feat: Setup basic CRUD (U not yet working) with function overloading

---
+++ src/main/react/se/kry/codetest/utils/fetchService.ts
@@ -0,0 +1,60 @@
+import { FETCH_ROUTE, SERVER_URL } from "../constants"
+import { Operation } from "../types/operation"
+import { Payload } from "../types/payload"
+
+export function fetchService(
+  method: Extract<Operation, "GET">,
+  controller: AbortController,
+): Promise<Response>
+
+export function fetchService(
+  method: Extract<Operation, "DELETE">,
+  controller: AbortController,
+  payload: Pick<Payload, "name">,
+): Promise<Response>
+
+export function fetchService(
+  method: Extract<Operation, "POST">,
+  controller: AbortController,
+  payload: Pick<Payload, "name" | "url">,
+): Promise<Response | null>
+
+export function fetchService(
+  method: Extract<Operation, "PUT">,
+  controller: AbortController,
+  payload: Pick<Payload, "name">,
+): Promise<Response | null>
+
+/**
+ *
+ * @param method Operation
+ * @param payload Depending on the Operation, might include or not the url.
+ * @returns Promisified HTTP Response or null
+ * @description Fetches the server with the proper operation and required payload
+ * and returns a valid response. Depending on the operation passed the fetching
+ * will vary.
+ * @fires window.fetch
+ * @emits O(1)
+ */
+export async function fetchService(
+  method: Operation,
+  controller: AbortController,
+  payload?: Partial<Payload>,
+) {
+  try {
+    const response = await window.fetch(`${SERVER_URL}/${FETCH_ROUTE}`, {
+      method,
+      body: JSON.stringify(payload),
+      signal: controller.signal,
+    })
+
+    return response
+  } catch (error) {
+    if (!controller.signal.aborted) {
+      // eslint-disable-next-line no-console
+      // Send to DataDog or similar
+      console.error({ error, method, payload })
+    }
+  }
+  return null
+}
